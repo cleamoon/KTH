9d7daf82a0e7e13d364891bb5da7d4688653cd7b
Yue Jiao <yj@kth.se>
2018-09-28 13:22:35 +0200

    Update files

---
+++ Rymdmiljö_och_satellit/labs/radiation/DosradSat/Systema-4.5.0.bat
@@ -0,0 +1,10 @@
+@echo off
+echo "==== Execution of Systema 4.5.0 ===="
+
+set SYSTEMA_WORKING_DIR=C:\GettingStarted\DosradSat
+echo SYSTEMA_HOME = %SYSTEMA_HOME%
+echo SYSTEMA_WORKING_DIR = %SYSTEMA_WORKING_DIR%
+"%SYSTEMA_HOME%\SystemaWIN.exe"
+echo "==== End of execution of Systema ===="
+
+
